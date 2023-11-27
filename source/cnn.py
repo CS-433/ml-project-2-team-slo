c7d68dc15069a092b55e02fd90d357aa8d2420f6
fpalmisa <fabio.palmisano@epfl.ch>
2023-11-27 11:55:51 +0100

    add_plot

--- source/cnn.py
+++ source/cnn.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from constants import *
 from helpers import *
+import matplotlib.pyplot as plt
 
 class Basic_CNN(nn.Module):
     def __init__(self, patch_size):
@@ -34,8 +35,11 @@ class Basic_CNN(nn.Module):
         x = self.fc2(x)
         return x
 
-    
+   
     def train_model(self, optimizer, criterion, train_loader, val_loader, num_epochs=10):
+        losses = []
+        F1_score = []
+        acc = []
         for epoch in range(num_epochs):
             self.train()
             for input, target in train_loader:
@@ -67,6 +71,19 @@ class Basic_CNN(nn.Module):
                 accuracy = total_correct / len(val_loader.dataset)
                 f1 = f1_score(torch.cat(tot_preds).numpy(), torch.cat(tot_targets).numpy())
                 print(f'Epoch {epoch+1}/{num_epochs}, Loss: {test_loss}, Validation Accuracy: {accuracy:.4f}, F1 score: {f1:.4f}')
+                losses.append(test_loss)
+                F1_score.append(f1)
+                acc.append(accuracy)
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+        axes[0].plot(range(num_epochs),losses)
+        axes[0].set_title('Loss')
+        axes[1].plot(range(num_epochs),F1_score)
+        axes[1].set_title('F1 score')
+        axes[2].plot(range(num_epochs),acc)
+        axes[2].set_title('Accuracy')
+        plt.show()
+
+
 
 
     def predict(self, test_loader):
@@ -79,3 +96,11 @@ class Basic_CNN(nn.Module):
                 output = (output > FOREGROUND_THRESHOLD).float()
                 predictions.append(output)
         return torch.cat(predictions).numpy()
+    
+
+
+    
+
+
+        
+
