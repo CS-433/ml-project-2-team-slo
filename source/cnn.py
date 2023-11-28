b5634ef2d788c556f8fd1470423fac537139aa7e
Larouyan <ylaaroussi@ymail.com>
2023-11-28 14:38:00 +0100

    update

--- source/cnn.py
+++ source/cnn.py
@@ -4,33 +4,40 @@ from constants import *
 from helpers import *
 
 class Basic_CNN(nn.Module):
-    def __init__(self, patch_size):
+    def __init__(self, patch_size, dropout_rate=0.8):
         super(Basic_CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
 
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)   
         self.relu2 = nn.ReLU()
         self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
 
-        # self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        # self.relu3 = nn.ReLU()
-        # self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.AvgPool2d(kernel_size=2, stride=2)
 
         # self.fc1 = nn.Linear(128 * (patch_size // 8) * (patch_size // 8), 128)
+        # self.dropout = nn.Dropout(p=dropout_rate)
         # self.fc2 = nn.Linear(128, 1)
-        self.fc1 = nn.Linear(64 * (patch_size // 4) * (patch_size // 4), 64)
-        self.fc2 = nn.Linear(64, 1)
+
+        self.fc1 = nn.Linear(128 * (patch_size // 8) * (patch_size // 8), 128)
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.fc2 = nn.Linear(128, 1)
         
 
     def forward(self, x):
         """Forward pass."""
-        x = self.pool1(self.relu1(self.conv1(x)))
-        x = self.pool2(self.relu2(self.conv2(x)))
-        # x = self.pool3(self.relu3(self.conv3(x)))
+        x = self.pool1(self.relu1(self.bn1(self.conv1(x))))
+        x = self.pool2(self.relu2(self.bn2(self.conv2(x))))
+        x = self.pool3(self.relu3(self.bn3(self.conv3(x))))
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
 
