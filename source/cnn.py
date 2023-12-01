6e16cd9d8497c9eecd636fbaa6293228c9dfcbba
fpalmisa <fabio.palmisano@epfl.ch>
2023-12-01 16:33:45 +0100

    changingModel

--- source/cnn.py
+++ source/cnn.py
@@ -9,28 +9,29 @@ class Basic_CNN(nn.Module):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         super(Basic_CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.BatchNorm1 = nn.BatchNorm2d(32)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.BatchNorm2 = nn.BatchNorm2d(64)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.BatchNorm3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(128 * (patch_size // 8) * (patch_size // 8), 128)
+        self.fc2 = nn.Linear(128, 1)
 
-        # self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        # self.relu3 = nn.ReLU()
-        # self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # self.fc1 = nn.Linear(128 * (patch_size // 8) * (patch_size // 8), 128)
-        # self.fc2 = nn.Linear(128, 1)
-        self.fc1 = nn.Linear(64 * (patch_size // 4) * (patch_size // 4), 64)
-        self.fc2 = nn.Linear(64, 1)
+
         
 
     def forward(self, x):
         """Forward pass."""
-        x = self.pool1(self.relu1(self.conv1(x)))
-        x = self.pool2(self.relu2(self.conv2(x)))
-        # x = self.pool3(self.relu3(self.conv3(x)))
+        x = self.pool1(self.relu1(self.BatchNorm1(self.conv1(x))))
+        x = self.pool2(self.relu2(self.BatchNorm2(self.conv2(x))))
+        x = self.pool3(self.relu3(self.BatchNorm3(self.conv3(x))))
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.fc2(x)
