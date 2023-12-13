f356bebfac6b8567b5c6d413984d0d083d824e5e
Larouyan <ylaaroussi@ymail.com>
2023-12-13 10:08:17 +0100

    update file paths using os.path.join

--- source/constants.py
+++ source/constants.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # -*- author : Vincent Roduit -*-
 # -*- date : 2023-11-25 -*-
-# -*- Last revision: 2023-12-03 -*-
+# -*- Last revision: 2023-12-13 (Yannis Laaroussi) -*-
 # -*- python version : 3.11.6 -*-
 # -*- Regroup all the constants used -*-
 
@@ -11,13 +11,13 @@ import torch
 
 #File paths
 ROOT_DIR = "../data/"
-TRAIN_DIR = ROOT_DIR + "training/"
-TEST_DIR = ROOT_DIR + "test_set_images/"
+TRAIN_DIR = os.path.join(ROOT_DIR, "training/")
+TEST_DIR = os.path.join(ROOT_DIR, "test_set_images/")
 IMAGE_DIR = os.path.join(TRAIN_DIR, "images/")
-GT_DIR = TRAIN_DIR + "groundtruth/"
+GT_DIR = os.path.join(TRAIN_DIR, "groundtruth/")
 MODELS_DIR = "../models/"   
-RESULTS_FOLDER_PATH = "../data/submission/images/"
-SUBMISSION_PATH = "../data/submission/"
+RESULTS_FOLDER_PATH = os.path.join(ROOT_DIR, "submission/images/")
+SUBMISSION_PATH = os.path.join(ROOT_DIR, "submission/")
 
 #Image parameters
 PATCH_SIZE = 16
